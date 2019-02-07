26e583e6605f17938f9990e4581cb106b7accc24
porsc <porschemaxcooper@hotmail.com>
2019-02-07 17:26:47 -0600

    xd

---
+++ app/src/main/java/android/itesm/edu/xd/MainActivity.java
@@ -0,0 +1,14 @@
+package android.itesm.edu.xd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        databaseList();
+    }
+}
