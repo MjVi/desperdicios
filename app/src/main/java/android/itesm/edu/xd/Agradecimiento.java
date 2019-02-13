8955480a9effc31f7076447395f94e50a2358d7c
MjVi <ma.jose.villagomez@gmail.com>
2019-02-13 14:19:34 -0600

    v2

--- app/src/main/java/android/itesm/edu/xd/Agradecimiento.java
+++ app/src/main/java/android/itesm/edu/xd/Agradecimiento.java
@@ -1,8 +1,10 @@
 package android.itesm.edu.xd;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class Agradecimiento extends AppCompatActivity {
 
@@ -17,8 +19,10 @@ public class Agradecimiento extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(tittle);
         getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
-
-
     }
 
+    public void goSetType (View view){
+        Intent intent = new Intent(this, TipoDCuenta.class);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/android/itesm/edu/xd/Donde.java
+++ app/src/main/java/android/itesm/edu/xd/Donde.java
@@ -1,8 +1,10 @@
 package android.itesm.edu.xd;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class Donde extends AppCompatActivity {
 
@@ -17,7 +19,10 @@ public class Donde extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(tittle);
         getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+    }
 
-
+    public void goBuscar(View view){
+    Intent intent = new Intent(this, Busqueda.class);
+    startActivity(intent);
     }
 }
